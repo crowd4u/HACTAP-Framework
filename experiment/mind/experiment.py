2fe478ca847785e41c380775a23f219142ece96f
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2020-08-23 04:56:15 +0900

    wip

--- experiment/mind/experiment.py
+++ experiment/mind/experiment.py
@@ -2,32 +2,27 @@ import torch
 import pandas as pd
 import argparse
 import torchvision
-from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 from modAL.models import ActiveLearner
-from modAL.uncertainty import uncertainty_sampling
 import torchvision.models as models
 from skorch import NeuralNetClassifier
 
 from hactap import solvers
 from hactap.tasks import Tasks
 from hactap.ai_worker import AIWorker
-from hactap.utils import random_strategy, ImageFolderWithPaths
+from hactap.utils import ImageFolderWithPaths
 from hactap.reporter import Reporter
 from hactap.human_crowd import get_labels_from_humans
 
 from mind_ai_worker import MindAIWorker
 
 
-# DATASET_PATH = '~/Google Drive/snippets/mind_dataset/dataset_mind'
-DATASET_PATH = '~/dataset_mind'
-
-height = 122  # int(500*0.2)
-width = 110  # int(455*0.2)
+DATASET_PATH = './dataset'
+height = 122
+width = 110
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--solver', default='gta')
-parser.add_argument('--task_size', default=500, type=int)
 parser.add_argument('--quality_requirements', default=0.8, type=float)
 parser.add_argument('--human_crowd_batch_size', default=200, type=int)
 parser.add_argument('--group_id', default='default')
@@ -36,9 +31,6 @@ parser.add_argument('--significance_level', default=0.05, type=float)
 
 
 def main():
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-
     args = parser.parse_args()
     reporter = Reporter(args)
 
@@ -51,52 +43,55 @@ def main():
         ])
     )
 
-    dataloader = DataLoader(
-        dataset=mind_dataset,
-        batch_size=args.task_size,
-        shuffle=True
-    )
-    X_root, y_root, idx_root = next(iter(dataloader))
-    tasks = Tasks(X_root, y_root, indexes=idx_root)
+    print("dataset size", len(mind_dataset))
+    data_index = range(len(mind_dataset))
+    human_labelable_index = []
+    # human_labelable_timestamp = []
+    image_paths = []
+    # label_order = pd.read_csv('label_order.csv')
 
+    for index in range(len(mind_dataset)):
+        path, label = mind_dataset.get_label(index)
+        image_paths.append(path)
+        if label != 3:
+            human_labelable_index.append(index)
+    #         human_labelable_timestamp.append(int(label_o
+    # rder[label_order['path'] == path]['created_at']))
+
+    # #TODO:
+    # # sort human_labelable_index by human_labelable_timestamp
+    # # add human crowd order mode
+
+    # print(human_labelable_timestamp)
+    # human_labelable_timestamp, human_labelable_index = zip(*
+    # sorted(zip(human_labelable_timestamp, human_labelable_index)))
+
+    print('human_labelable_index', len(human_labelable_index))
+    tasks = Tasks(mind_dataset, data_index, human_labelable_index)
     get_labels_from_humans(tasks, args.human_crowd_batch_size)
 
     # Prepare AI workers
-    if args.solver == 'al':
-        query_strategy = uncertainty_sampling
-    else:
-        query_strategy = random_strategy
-
-    X_train, y_train = tasks.train_set
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
     ai_workers = [
-        AIWorker(
-            ActiveLearner(
-                estimator=MindAIWorker(),
-                query_strategy=query_strategy
-            )
-        ),
-        AIWorker(
-            ActiveLearner(
-                estimator=NeuralNetClassifier(
-                    models.resnet18(), device=device
-                ),
-                query_strategy=query_strategy
-            )
-        ),
-        AIWorker(
-            ActiveLearner(
-                estimator=NeuralNetClassifier(
-                    models.mobilenet_v2(), device=device
-                ),
-                query_strategy=query_strategy
-            )
-        )
+        AIWorker(MindAIWorker()),
+        AIWorker(NeuralNetClassifier(
+            models.resnet18(),
+            device=device,
+            train_split=None
+        ))
+    ]
+
+    al_ai_workers = [
+        AIWorker(ActiveLearner(
+            estimator=MindAIWorker()
+        ))
     ]
 
     if args.solver == 'al':
         solver = solvers.AL(
             tasks,
-            ai_workers,
+            al_ai_workers,
             args.quality_requirements,
             args.human_crowd_batch_size,
             reporter=reporter
@@ -115,7 +110,7 @@ def main():
 
     output_df = pd.DataFrame(
         {
-            'index': output.raw_indexes,
+            'index': image_paths,
             'y_human': output.raw_y_human,
             'y_ai': output.raw_y_ai,
             'ground_truth': output.raw_ground_truth
