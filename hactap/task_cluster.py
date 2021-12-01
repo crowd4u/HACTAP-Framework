a00dfcaed19226c3db0bbbb991f5554c4be8177b
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2021-12-01 16:52:28 +0900

    add update status 2 for wf

--- hactap/task_cluster.py
+++ hactap/task_cluster.py
@@ -16,10 +16,12 @@ class TaskCluster:
     def __init__(
         self,
         model: BaseAIWorker,
+        aiw_id: int,
         info: Dict[str, Dict],
         prior_distribution: List[int] = [1, 1]
     ):
         self.__model = model
+        self.__aiw_id = aiw_id
         self.__info = info
         self.__prior_distribution = prior_distribution
 
@@ -43,6 +45,10 @@ class TaskCluster:
     @property
     def conflict_rate_with_human(self) -> int:
         return self.__conflict_rate_with_human
+
+    @property
+    def aiw_id(self) -> Dict:
+        return self.__aiw_id
 
     @property
     def rule(self) -> Dict:
@@ -257,3 +263,74 @@ class TaskCluster:
     @property
     def bata_dist(self) -> List:
         return self.__bata_dist
+
+    def update_status2(
+        self,
+        test_indexes,
+        assignable_indexes,
+        n_monte_carlo_trial: int = 1
+    ) -> None:
+        self.__n_answerable_tasks = 0
+        self.__assignable_task_indexes = []
+        self.__y_pred = []
+
+        if len(test_indexes) == 0:
+            self.__assignable_task_idx_test = []
+            self.__match_rate_with_human = 0
+            self.__conflict_rate_with_human = 0
+            return
+
+        # 現在の dataset.assignable_indexes に基づいて、そのタスククラスタに割り当て可能なindexesを更新する
+
+        for answerable_tasks_id, y_pred_i in zip(self.__info["stat"]["y_pred_remain_ids"], self.__info["stat"]["y_pred_remain"]): # NOQA
+            if answerable_tasks_id in assignable_indexes:
+                self.__n_answerable_tasks += 1
+
+                self.__assignable_task_indexes.append(answerable_tasks_id)
+                self.__y_pred.append(self.rule['rule']['to'])
+
+        y_pred_test = []
+        y_pred_test_human = []
+        y_pred_test_ids = []
+        for answerable_tasks_id, y_pred_i, y_pred_i_human in zip(self.__info["stat"]["y_pred_test_ids"], self.__info["stat"]["y_pred_test"], self.__info["stat"]["y_pred_test_human"]): # NOQA
+            if answerable_tasks_id in test_indexes:
+                y_pred_test.append(self.rule['rule']['to'])
+                y_pred_test_human.append(y_pred_i_human)
+                y_pred_test_ids.append(answerable_tasks_id)
+
+        self.__assignable_task_idx_test = y_pred_test_ids
+        self.__assignable_task_idx_train = self.__info["stat"]["y_pred_train_ids"] # NOQA
+
+        if len(y_pred_test_ids) != 0:
+            # 人間のラベルを参照する
+            # y_human = np.array([y for x, y in iter(human_ds)])
+
+            self.__test_y_human = y_pred_test_human
+            self.__test_y_predict = y_pred_test
+
+            self.__train_y_human = self.__info["stat"]["y_pred_train_human"]
+            self.__train_y_predict = [self.rule['rule']['to']] * len(self.__info["stat"]["y_pred_train"]) # NOQA
+
+            # 一致回数と不一致回数を計算する
+            self.__match_rate_with_human = 0
+
+            for _p, _h in zip(
+                y_pred_test,
+                y_pred_test_human
+            ):
+                # for _p, _h in zip(y_preds_test, y_human_test):
+                if int(_p) == int(_h):
+                    self.__match_rate_with_human += 1
+
+            self.__conflict_rate_with_human = len(y_pred_test) - self.__match_rate_with_human # NOQA
+
+            # ベータ分布に従う乱数を生成する
+            self.__bata_dist = []
+
+            for x in range(int(n_monte_carlo_trial / 100_000)):
+                self.__bata_dist.extend(beta.rvs(
+                    (self.__prior_distribution[0] + self.__match_rate_with_human), # NOQA
+                    (self.__prior_distribution[1] + self.__conflict_rate_with_human), # NOQA
+                    size=100_000
+                ))
+            return
